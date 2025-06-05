f6c6961e54489dba8cc2698aa8cac1e1ac4a12ce
Noam Yan <noamyan9@gmail.com>
2025-06-05 13:38:41 -0500

    init

---
+++ history-serivce/src/main/java/com/bfsforum/historyserivce/HistorySerivceApplication.java
@@ -0,0 +1,15 @@
+package com.bfsforum.historyserivce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class HistorySerivceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HistorySerivceApplication.class, args);
+    }
+
+}
---
+++ history-serivce/src/test/java/com/bfsforum/historyserivce/HistorySerivceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bfsforum.historyserivce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HistorySerivceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
