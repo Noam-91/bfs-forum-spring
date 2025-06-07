052815223213c75d45b8882f61d3f31b2f5012f7
Noam Yan <noamyan9@gmail.com>
2025-06-06 23:55:56 -0500

    Add exception handler and custom exceptions

---
+++ message-service/src/main/java/com/bfsforum/messageservice/exception/GlobalExceptionHandler.java
@@ -0,0 +1,33 @@
+package com.bfsforum.messageservice.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<Object> handleNotFoundException(NotFoundException ex) {
+        log.error("NotFoundException: {}", ex.getMessage());
+        return new ResponseEntity<>(Map.of("error",ex.getMessage()), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(NotAuthorizedException.class)
+    public ResponseEntity<Object> handleNotAuthorizedException(NotAuthorizedException ex) {
+        log.error("NotAuthorizedException: {}", ex.getMessage());
+        return new ResponseEntity<>(Map.of("error",ex.getMessage()), HttpStatus.FORBIDDEN);
+    }
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<Object> handleBadRequestException(BadRequestException ex) {
+        log.error("BadRequestException: {}", ex.getMessage());
+        return new ResponseEntity<>(Map.of("error", ex.getMessage()), HttpStatus.BAD_REQUEST);
+    }
+}
