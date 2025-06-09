c44eff778d6b22aee296540b6ed8b47d547ea3ac
Noam Yan <noamyan9@gmail.com>
2025-06-09 09:36:18 -0500

    Change the id datatype back to String

--- email-service/src/main/java/com/bfsforum/emailservice/domain/VerificationToken.java
+++ email-service/src/main/java/com/bfsforum/emailservice/domain/VerificationToken.java
@@ -1,12 +1,11 @@
 package com.bfsforum.emailservice.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 import org.hibernate.annotations.UuidGenerator;
 
 import java.time.Instant;
@@ -17,20 +16,21 @@ import java.util.UUID;
 @Data
 @Entity
 @Table(name = "verification")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class VerificationToken {
 
     @Id
-    @Column(length = 36)
     private String token;
-    @UuidGenerator
-    @Column(name = "user_id", nullable = false, columnDefinition = "BINARY(16)")
-    private UUID userId;
 
-    @Column(name = "created_at", nullable = false)
+    @Column(name = "user_id", nullable = false)
+    private String userId;
+
+    @JsonIgnore
+    @Column(name = "created_at", insertable = false, updatable = false)
     private Instant createdAt;
 
-    @Column(name = "expired_at")
+    @Column(name = "expired_at", insertable = false, updatable = false)
     private Instant expiredAt;
-
-
 }
