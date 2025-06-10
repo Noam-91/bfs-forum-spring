5c1f9ffe3536b1500245c755006a4dfc0257c24e
Noam Yan <noamyan9@gmail.com>
2025-06-10 13:07:37 -0500

    Test case not implemented reminder

---
+++ post-service/src/test/java/com/bfsforum/postservice/controller/PostControllerTest.java
@@ -0,0 +1,36 @@
+package com.bfsforum.postservice.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PostControllerTest {
+//todo: test cases!!
+  @Test
+  void getQueriedPosts() {
+  }
+
+  @Test
+  void createPost() {
+  }
+
+  @Test
+  void getPostById() {
+  }
+
+  @Test
+  void updatePost() {
+  }
+
+  @Test
+  void replyPost() {
+  }
+
+  @Test
+  void transferPostStatus() {
+  }
+
+  @Test
+  void toggleReplyActive() {
+  }
+}
