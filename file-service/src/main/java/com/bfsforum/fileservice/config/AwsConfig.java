64742d68d6e17ee53d24eb239c9988610062592c
Noam Yan <noamyan9@gmail.com>
2025-06-12 19:04:18 -0500

    Hide secret

--- file-service/src/main/java/com/bfsforum/fileservice/config/AwsConfig.java
+++ file-service/src/main/java/com/bfsforum/fileservice/config/AwsConfig.java
@@ -1,5 +1,6 @@
 package com.bfsforum.fileservice.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
@@ -10,12 +11,21 @@ import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 @Configuration
 public class AwsConfig {
 
+    @Value("${AWS_ACCESS_KEY_ID}")
+    private String awsAccessKeyId;
+
+    @Value("${AWS_SECRET_ACCESS_KEY}")
+    private String awsSecretAccessKey;
+
+    @Value("${AWS_REGION}")
+    private String awsRegion;
+
     @Bean
     public S3Presigner s3Presigner() {
         return S3Presigner.builder()
-            .region(Region.US_EAST_2) // ✅ Your region here
+            .region(Region.of(awsRegion))
             .credentialsProvider(StaticCredentialsProvider.create(
-                AwsBasicCredentials.create("AKIAQ54B6CQQE6AJQIEJ", "jG/BGiN8OBHPqrZaslUsd+B+Y86d1fsLq5aolt+z")
+                AwsBasicCredentials.create(awsAccessKeyId, awsSecretAccessKey)
             ))
             .build();
     }
